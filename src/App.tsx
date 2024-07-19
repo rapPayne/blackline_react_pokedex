a5f0b155756e6f09c14f23eba7250258a36f5af4
rapPayne <bob@creator.net>
2024-07-19 11:16:19 -0500

    Merged

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,30 @@ import PokedexRoot from "./pages/PokedexRoot";
 import { NavLink, Navigate, Route, Routes } from "react-router-dom";
 import { PokemonPage } from "./pages/PokemonPage";
 function App() {
-	return (
-		<>
-			<header>
-				<nav>
-					<NavLink to="/">Pokedex</NavLink>
-					<NavLink to="/about">About us</NavLink>
-					<NavLink to="/contact">Contact us</NavLink>
-				</nav>
-			</header>
+  return (
+    <>
+      <header>
+        <nav>
+          <NavLink to="/">Pokedex</NavLink>
+          <NavLink to="/about">About us</NavLink>
+          <NavLink to="/contact">Contact us</NavLink>
+        </nav>
+      </header>
 
-			<Routes>
-				<Route path="/" element={<PokedexRoot />} />
-				<Route path="/pokemons" element={<Navigate to="/" />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/contact" element={<Contact />} />
-				<Route path="/pokemon/:name" element={<PokemonPage />} />
-				<Route path="*" element={<FourOhFour />} />
-			</Routes>
+      <Routes>
+        <Route path="/" element={<Navigate to="/pokemon" />} />
+        <Route path="/pokemon" element={<PokedexRoot />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/pokemon/:name" element={<PokemonPage />} />
+        <Route path="*" element={<FourOhFour />} />
+      </Routes>
 
-			<footer>
-				Copyright &copy; Blackline, Inc. {new Date().getFullYear()}
-			</footer>
-		</>
-	);
+      <footer>
+        Copyright &copy; Blackline, Inc. {new Date().getFullYear()}
+      </footer>
+    </>
+  )
 }
 
 export default App;
